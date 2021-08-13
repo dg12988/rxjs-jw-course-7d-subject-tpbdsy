fc5e01f6ea973457352f9fb777cb95d46a988e2d
dg12988 <doug.goldberg@me.com>
2021-08-13 10:44:59 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { fromEvent, Subject } from "rxjs";
+import { map } from "rxjs/operators";
 
 const emitButton = document.querySelector('button#emit');
 const inputElement: HTMLInputElement = document.querySelector('#value-input');
@@ -6,9 +7,9 @@ const subscribeButton = document.querySelector('button#subscribe');
 
 const value$ = new Subject<string>();
 
-fromEvent(emitButton, 'click').subscribe(
-  () => value$.next(inputElement.value)
-);
+fromEvent(emitButton, 'click').pipe(
+  map(() => inputElement.value)
+).subscribe(value$);
 
 fromEvent(subscribeButton, 'click').subscribe(
   () => {
